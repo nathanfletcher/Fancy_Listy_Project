3fc3a51f1e246cf6a650fd5a9e55632456d65228
KobbyFletcher <fletcher.nathan.a@gmail.com>
2016-01-18 12:36:58 +0000

    Initial commit

---
+++ app/src/main/java/com/ketecode/fancylistyproject/MainActivity.java
@@ -0,0 +1,122 @@
+package com.ketecode.fancylistyproject;
+
+import android.app.FragmentManager;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //Initial fragment code
+        FragmentManager fragmentManager = getFragmentManager();
+        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        //calling the first menu item's fragment to be displayed using FragmentManager class
+        FirstFragment frag1 = new FirstFragment();
+        fragmentTransaction.add(R.id.fragment_container, frag1, "Our First Fragment");
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        //Declaring and initializing fragment classes
+        FragmentManager fragmentManager = getFragmentManager();
+        android.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        FirstFragment frag1 = new FirstFragment();
+        SecondFragment frag2 = new SecondFragment();
+        ThirdFragment frag3 = new ThirdFragment();
+
+        if (id == R.id.nav_camera) {
+            fragmentTransaction.replace(R.id.fragment_container, frag1, "Our First Fragment");
+        } else if (id == R.id.nav_gallery) {
+            fragmentTransaction.replace(R.id.fragment_container, frag2, "Our Second Fragment");
+        } else if (id == R.id.nav_slideshow) {
+            fragmentTransaction.replace(R.id.fragment_container, frag3, "Our Third Fragment");
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        fragmentTransaction.commit();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/ketecode/fancylistyproject/ThirdFragment.java
@@ -0,0 +1,31 @@
+package com.ketecode.fancylistyproject;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by KobbyFletcher on 1/18/16.
+ */
+public class ThirdFragment extends Fragment {
+
+    TextView someTextView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /*After you override this method Android Studio will write the line below for you.
+        Delet it. or comment it out. You don't need it.
+         */
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        View view = inflater.inflate(R.layout.fragment2_layout, container, false);
+        someTextView = (TextView) view.findViewById(R.id.fragmentTitle);
+        someTextView.setText("Third Fragment !!!");
+        return view;
+    }
+}
